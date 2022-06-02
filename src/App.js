8c13e873a238cbdd348a77542499128db0dcbcfe
rachana1792 <rachana.neoito@gmail.com>
2022-06-02 14:32:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import profile from './profile'
+export default function App() {
+  return (
+    <div>
+      <profile/>
+    </div>
+  )
+}
---
+++ src/profile.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const profile = () => {
+  return (
+   <p>Hello from profile </p>
+  )
+}
+export default profile
