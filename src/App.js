9859230c3bab0d594225579af5fb2f7db1e96763
joyalneo <joyal@neoito.com>
2022-06-17 18:27:11 +0530

    bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Profile from './profile/profile.js'
-import './assets/css/main.scss';
+// import './assets/css/main.scss';
 
 export default function App() {
   return (
